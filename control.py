Changed omega to thetae (electrical angle) in the bemf calculation. Minor fixes to normalize_angle usage and pole calculation.
git-svn-id: svn+ssh://repl.esden.net/svn/obldc-sim/trunk@9 a197d47a-b172-4465-b25f-b54fd22ab3e4

control.py
@@ -2,7 +2,9 @@ import numpy as np
 
 import dyn_model  as dm
 
-PWM_freq = 1000
+import misc_utils as mu
+
+PWM_freq = 8000
 
 
 #
@@ -11,7 +13,7 @@ PWM_freq = 1000
 #
 def run(Sp, Y, t):
 
-    elec_angle = norm_angle(Y[ov_theta]*dm.NbPoles/3) 
+    elec_angle = mu.norm_angle(Y[dm.ov_theta]*dm.NbPoles/3) 
     
 
     U = np.zeros(dm.iv_size)