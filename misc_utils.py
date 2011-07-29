4a9437ba1632499b22536714149381b27736c9b9
Piotr Esden-Tempski <piotr@esden.net>
2011-07-29 20:32:17 +0000

    Changed angle normalization to wrap around when the input angle is negative.
    git-svn-id: svn+ssh://repl.esden.net/svn/obldc-sim/trunk@15 a197d47a-b172-4465-b25f-b54fd22ab3e4

--- misc_utils.py
+++ misc_utils.py
@@ -8,4 +8,8 @@ def deg_of_rad(r): return r*180./math.pi
 #
 def norm_angle(alpha):
     alpha_n = math.fmod(alpha, 2*math.pi)
+
+    if alpha_n < 0.:
+        alpha_n = (2*math.pi) + alpha_n
+
     return alpha_n
